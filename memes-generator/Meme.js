6c8471051e805b201491ad924cf72b02f68ebec8
Simona Marcu <67797490+simonamarcu2@users.noreply.github.com>
2022-02-08 12:25:38 +0000

    Added API and packages

--- memes-generator/Meme.js
+++ memes-generator/Meme.js
@@ -1,10 +1,57 @@
+import React, {useState, useEffect} from 'react'
+import "./style.css";
 
 const Meme = ({meme, setMeme})=>{
+    const [inp, setInp] = useState({
+        template_id: meme.id,
+        username: "memes",
+        password: "ShowMeMemes",
+        boxes: []
+    })
+
+    const [download, setDownload] = useState(null)
+    const [loading, setLoading] = useState(false)
+    const generateMeme = ()=>{
+        setLoading(true)
+        let url = `https://api.imgflip.com/caption_image?template_id=${inp.template_id}&username=${inp.username}&password=${inp.password}`;
+        inp.boxes.map((box, index) => {
+        url += `&boxes[${index}][text]=${box.text}`;
+        });
+        fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+            setMeme({ ...meme, url: data.data.url })
+            setDownload(data.data.url)
+            setLoading(false)
+        })
+        .catch(err => {
+            console.log(err);
+        });
+    }
+    const downloadMeme = ()=> {
+    fetch(download, {
+        method: "GET",
+        headers: {}
+    })
+        .then(response => {
+        response.arrayBuffer().then(function(buffer) {
+            const url = window.URL.createObjectURL(new Blob([buffer]));
+            const link = document.createElement("a");
+            link.href = url;
+            link.setAttribute("download", "meme.png"); 
+            document.body.appendChild(link);
+            link.click();
+        });
+        })
+        .catch(err => {
+        console.log(err);
+        });
+    }
 
     return(
         <div className="meme-container">
             <h1>Meme</h1>
-        <div>
+            {loading===true? <div>Generating your Meme</div>: <div>
             <img src={meme.url} alt={meme.id} className="img"/>
             {
             [...Array(meme.box_count)].map((_, index) =>(
@@ -12,21 +59,23 @@ const Meme = ({meme, setMeme})=>{
                 placeholder={`Enter Input ${index+1}`}
                 className="input" 
                 key={index}
+                onChange={(e) => {
+                    const newBoxes = inp.boxes;
+                    newBoxes[index] = { text: e.target.value };
+                    setInp({ ...inp, boxes: newBoxes });
+                }}
                 />
             ))
             }
             <div>
-            <button>Generate Meme</button>
-            <button>Back</button>
+            <button onClick={()=>{generateMeme()}}>Generate Meme</button>
+            <button onClick={()=>{setMeme(null)}}>Back</button>
+            {download === null?<></>: <button onClick={e => downloadMeme(e)}>Download</button>}
             </div>
             </div>
+            }
+
         </div>
     )
 }
-
-const [inp, setInp] = useState({
-    template_id: meme.id,
-    username: "username",
-    password: "password",
-    boxes: []
-})
+export default  Meme;
